4f0559d4e006907b393571d205e6c92465e25d4e
Naumov Ilya <60252020@lemanapro.ru>
2025-05-30 15:15:00 +0300

    update readme and add meta

--- inc/header.php
+++ inc/header.php
@@ -10,6 +10,7 @@ if (session_status() === PHP_SESSION_NONE) {
 	<meta charset="UTF-8">
 	<title>Грузоперевозки</title>
 	<link rel="stylesheet" href="assets/css/style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body>
